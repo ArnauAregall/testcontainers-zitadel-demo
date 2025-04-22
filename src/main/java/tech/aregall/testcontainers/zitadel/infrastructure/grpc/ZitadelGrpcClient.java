409d53040004e9ad61bcf7272139022910df9b76
Arnau Aregall <arnau.are@gmail.com>
2025-04-22 06:32:45 +0200

    chore(grpc) Healtcheck for Zitadel gRPC server when app is ready

--- src/main/java/tech/aregall/testcontainers/zitadel/infrastructure/grpc/ZitadelGrpcClient.java
+++ src/main/java/tech/aregall/testcontainers/zitadel/infrastructure/grpc/ZitadelGrpcClient.java
@@ -1,20 +1,42 @@
 package tech.aregall.testcontainers.zitadel.infrastructure.grpc;
 
+import io.grpc.StatusRuntimeException;
 import net.devh.boot.grpc.client.inject.GrpcClient;
+import org.slf4j.Logger;
+import org.springframework.boot.context.event.ApplicationReadyEvent;
+import org.springframework.context.event.EventListener;
 import org.springframework.stereotype.Service;
 import zitadel.admin.v1.Admin;
 import zitadel.admin.v1.AdminServiceGrpc;
+import zitadel.instance.v1.InstanceOuterClass.InstanceDetail;
+
+import static java.util.Objects.nonNull;
+import static org.slf4j.LoggerFactory.getLogger;
 
 @Service
 public class ZitadelGrpcClient {
 
+    private static final Logger log = getLogger(ZitadelGrpcClient.class);
+
     @GrpcClient("zitadel")
     private AdminServiceGrpc.AdminServiceBlockingStub adminServiceBlockingStub;
 
+    private InstanceDetail getInstance() {
+        return adminServiceBlockingStub.getMyInstance(Admin.GetMyInstanceRequest.newBuilder().build()).getInstance();
+    }
+
     public String getInstanceName() {
-        return adminServiceBlockingStub.getMyInstance(Admin.GetMyInstanceRequest.newBuilder().build())
-                .getInstance()
-                .getName();
+        return getInstance().getName();
+    }
+
+    @EventListener(ApplicationReadyEvent.class)
+    void onReady() {
+        try {
+            final var isHealthy = nonNull(adminServiceBlockingStub.healthz(Admin.HealthzRequest.newBuilder().build()));
+            log.debug("Zitadel gRPC client is healthy: {}", isHealthy);
+        } catch (StatusRuntimeException e) {
+            log.error("Failed to connect to Zitadel gRPC server", e);
+        }
     }
 
 }
